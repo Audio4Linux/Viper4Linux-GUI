7f0372c21e8e5e896c4ccc83c639ab6780c380e7
ThePBone <thebone.main@gmail.com>
2019-07-01 00:25:23 +0200

    Fixed line endings

--- uninstall-debian.sh
+++ uninstall-debian.sh
@@ -16,7 +16,8 @@ fi
 
 printf "This uninstall script should only be used if you have installed Viper4Linux-GUI using the installation-script for Debian/Ubuntu. (https://github.com/ThePBone/Viper4Linux-GUI#debianubuntu)\n"
 read -p "Do you want to continue? [y/n] " -n 1 -r
-echo    # (optional) move to a new line
+
+echo ""  # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
