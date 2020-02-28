f4c15802fedef306bde85d930e2c2c66590388ea
ThePBone <thebone.main@gmail.com>
2020-02-29 00:48:00 +0100

    Rewrite style helpers

--- misc/stylehelper.cpp
+++ misc/stylehelper.cpp
@@ -39,120 +39,30 @@ void StyleHelper::SetStyle(){
             f.open(QFile::ReadOnly | QFile::Text);
             QTextStream ts(&f);
             qApp->setStyleSheet(ts.readAll());
-            if(style_sheet=="amoled" || style_sheet=="vsdark"){
-                m_host->settings_dlg->updateButtonStyle(true);
-                QPixmap pix(":/icons/settings-white.svg");
-                QIcon icon(pix);
-                QPixmap pix2(":/icons/queue-white.svg");
-                QIcon icon2(pix2);
-                QPixmap pix3(":/icons/menu-white.svg");
-                QIcon icon3(pix3);
-                m_host->ui->set->setIcon(icon);
-                m_host->ui->cpreset->setIcon(icon2);
-                m_host->ui->toolButton->setIcon(icon3);
-            }else{
-                m_host->settings_dlg->updateButtonStyle(false);
-                QPixmap pix(":/icons/settings.svg");
-                QIcon icon(pix);
-                QPixmap pix2(":/icons/queue.svg");
-                QIcon icon2(pix2);
-                QPixmap pix3(":/icons/menu.svg");
-                QIcon icon3(pix3);
-                m_host->ui->set->setIcon(icon);
-                m_host->ui->cpreset->setIcon(icon2);
-                m_host->ui->toolButton->setIcon(icon3);
-            }
+            loadIcons(style_sheet=="amoled" || style_sheet=="vsdark");
         }
     }else{
-        loadIcons(false);
-        if(color_palette=="dark"){
-            loadIcons(true);
-            QColor background = QColor(53,53,53);
-            QColor foreground = Qt::white;
-            QColor base = QColor(25,25,25);
-            QColor selection = QColor(42, 130, 218);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }else if(color_palette=="gray"){
-            loadIcons(true);
-            QColor background = QColor(49,49,74);
-            QColor foreground = Qt::white;
-            QColor base = QColor(83,83,125);
-            QColor selection = QColor(85,85,127);
-            setPalette(base,background,foreground,selection,Qt::black,QColor(144,144,179));
-        }else if(color_palette=="white"){
-            QColor background = Qt::white;
-            QColor foreground = Qt::black;
-            QColor base = Qt::white;
-            QColor selection = QColor(56,161,227);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="blue"){
-            loadIcons(true);
-            QColor background = QColor(0,0,50);
-            QColor foreground = Qt::white;
-            QColor base = QColor(0,0,38);
-            QColor selection = QColor(85,0,255);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="darkblue"){
-            loadIcons(true);
-            QColor background = QColor(19,25,38);
-            QColor foreground = Qt::white;
-            QColor base = QColor(14,19,29);
-            QColor selection = QColor(70,79,89);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="honeycomb"){
-            QColor background = QColor(212,215,208);
-            QColor foreground = Qt::black;
-            QColor base = QColor(185,188,182);
-            QColor selection = QColor(243,193,41);
-            setPalette(base,background,foreground,selection,Qt::white);
-        }
-        else if(color_palette=="black"){
-            loadIcons(true);
-            QColor background = QColor(16,16,16);
-            QColor foreground = QColor(222,222,222);
-            QColor base = Qt::black;
-            QColor selection = QColor(132,132,132);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="darkgreen"){
-            loadIcons(true);
-            QColor background = QColor(27,34,36);
-            QColor foreground = QColor(197,209,217);
-            QColor base = QColor(30,30,30);
-            QColor selection = QColor(21,67,58);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="green"){
-            loadIcons(true);
-            QColor background = QColor(0,12,0);
-            QColor foreground = Qt::white;
-            QColor base = QColor(6,29,12);
-            QColor selection = QColor(86,191,121);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="stone"){
-            loadIcons(true);
-            QColor background = QColor(34,45,50);
-            QColor foreground = Qt::white;
-            QColor base = QColor(27,36,40);
-            QColor selection = QColor(165,206,255);
-            setPalette(base,background,foreground,selection,Qt::black);
-        }
-        else if(color_palette=="custom"){
+        auto palettes = ColorStyleProvider::TABLE();
+        if(color_palette=="custom"){
             QColor base = QColor(loadColor(0,0),loadColor(0,1),loadColor(0,2));
             QColor background = QColor(loadColor(1,0),loadColor(1,1),loadColor(1,2));
             QColor foreground = QColor(loadColor(2,0),loadColor(2,1),loadColor(2,2));
             QColor selection = QColor(loadColor(3,0),loadColor(3,1),loadColor(3,2));
             QColor disabled = QColor(loadColor(4,0),loadColor(4,1),loadColor(4,2));
             QColor selectiontext = QColor(255-loadColor(3,0),255-loadColor(3,1),255-loadColor(3,2));
+            ColorStyle cs = ColorStyle(m_appconf->getWhiteIcons(),
+                                       base,background,foreground,selection,selectiontext,disabled);
 
-            setPalette(base,background,foreground,selection,selectiontext,disabled);
+            setPalette(cs);
             loadIcons(m_appconf->getWhiteIcons());
         }
+        else if(palettes.contains(color_palette)){
+            ColorStyle currentColorStyle = palettes[color_palette];
+            setPalette(currentColorStyle);
+            loadIcons(currentColorStyle.useWhiteIcons);
+        }
         else{
+            loadIcons(false);
             QApplication::setPalette(m_host->style()->standardPalette());
             QFile f(":/default.qss");
             if (!f.exists())printf("Unable to set stylesheet, file not found\n");
@@ -167,24 +77,35 @@ void StyleHelper::SetStyle(){
     }
     emit styleChanged();
 }
-void StyleHelper::setPalette(const QColor& base,const QColor& background,const QColor& foreground,const QColor& selection,const QColor& selectiontext,const QColor& disabled){
+void StyleHelper::setPalette(const ColorStyle& s){
     QPalette *palette = new QPalette();
-    palette->setColor(QPalette::Window, background);
-    palette->setColor(QPalette::WindowText, foreground);
-    palette->setColor(QPalette::Base, base);
-    palette->setColor(QPalette::AlternateBase, background);
-    palette->setColor(QPalette::ToolTipBase, background);
-    palette->setColor(QPalette::ToolTipText, foreground);
-    palette->setColor(QPalette::Text, foreground);
-    palette->setColor(QPalette::Button, background);
-    palette->setColor(QPalette::ButtonText, foreground);
+    palette->setColor(QPalette::Window, s.background);
+    palette->setColor(QPalette::WindowText, s.foreground);
+    palette->setColor(QPalette::Base, s.base);
+    palette->setColor(QPalette::AlternateBase, s.background);
+    palette->setColor(QPalette::ToolTipBase, s.background);
+    palette->setColor(QPalette::ToolTipText, s.foreground);
+    palette->setColor(QPalette::Text, s.foreground);
+    palette->setColor(QPalette::Button, s.background);
+    palette->setColor(QPalette::ButtonText, s.foreground);
     palette->setColor(QPalette::BrightText, Qt::red);
     palette->setColor(QPalette::Link, QColor(42, 130, 218));
-    palette->setColor(QPalette::Highlight, selection);
-    palette->setColor(QPalette::HighlightedText, selectiontext);
+    palette->setColor(QPalette::Highlight, s.selection);
+    palette->setColor(QPalette::HighlightedText, s.selectiontext);
     qApp->setPalette(*palette);
-    QString rgbdisabled = disabled.name();
-    qApp->setStyleSheet(R"(QFrame[frameShape="4"], QFrame[frameShape="5"]{ color: gray; }*::disabled { color: )" + rgbdisabled +";}QToolButton::disabled { color: " + rgbdisabled +";}QComboBox::disabled { color: " + rgbdisabled +";}");
+    qApp->setStyleSheet(QString(R"(QFrame[frameShape="4"], QFrame[frameShape="5"]{
+                            color: gray;
+                        }
+                        *::disabled {
+                        color: %1;
+                        }
+                        QToolButton::disabled{
+                        color: %1;
+
+                        }
+                        QComboBox::disabled{
+                        color: %1;
+                        })").arg(s.disabled.name()));
 }
 void StyleHelper::loadIcons(bool white){
     MainWindow* m_host = qobject_cast<MainWindow*>(m_objhost);
@@ -229,12 +150,4 @@ int StyleHelper::loadColor(int index,int rgb_index){
     }
     QStringList rgb = elements[index].split(',');
     return rgb[rgb_index].toInt();
-}
-void StyleHelper::switchPalette(const QPalette& palette)
-{
-    MainWindow* m_host = qobject_cast<MainWindow*>(m_objhost);
-    qApp->setPalette(palette);
-    QList<QWidget*> widgets = m_host->findChildren<QWidget*>();
-    foreach (QWidget* w, widgets)
-        w->setPalette(palette);
 }
--- misc/stylehelper.h
+++ misc/stylehelper.h
@@ -16,23 +16,65 @@
 #define STYLEHELPER_H
 #include <QObject>
 #include <QColor>
+#include "misc/initializableqmap.h"
+#define CS_UNIT QString,ColorStyle
 
+class ColorStyle;
 class StyleHelper : public QObject
 {
     Q_OBJECT
 public:
+
     StyleHelper(QObject* host);
     void SetStyle();
     void loadIcons(bool white);
     int loadColor(int index,int rgb_index);
-    void switchPalette(const QPalette& palette);
-    void setPalette(const QColor& base,const QColor& background,
-                                 const QColor& foreground,const QColor& selection = QColor(42,130,218),
-                                 const QColor& selectiontext = Qt::black,const QColor& disabled = QColor(85,85,85));
+    void setPalette(const ColorStyle& s);
 private:
     QObject* m_objhost;
 signals:
     void styleChanged();
 };
 
+class ColorStyle {
+public:
+    bool useWhiteIcons;
+    QColor background;
+    QColor foreground;
+    QColor base;
+    QColor selection;
+    QColor selectiontext;
+    QColor disabled;
+
+    ColorStyle(){}
+    ColorStyle(const bool _whiteIcons, const QColor& _base,const QColor& _background,
+               const QColor& _foreground,const QColor& _selection = QColor(42,130,218),
+               const QColor& _selectiontext = Qt::black,const QColor& _disabled = QColor(85,85,85)){
+        useWhiteIcons = _whiteIcons;
+        base = _base;
+        background = _background;
+        foreground = _foreground;
+        selection = _selection;
+        selectiontext = _selectiontext;
+        disabled = _disabled;
+    }
+};
+
+
+namespace ColorStyleProvider {
+static QMap<CS_UNIT> TABLE(){
+    InitializableQMap<CS_UNIT> map;
+    map << QPair<CS_UNIT>("dark",ColorStyle(true,QColor(25,25,25),QColor(53,53,53),Qt::white,QColor(42, 130, 218)))
+        << QPair<CS_UNIT>("gray",ColorStyle(true,QColor(83,83,125),QColor(49,49,74),Qt::white,QColor(85,85,127),QColor(144,144,179)))
+        << QPair<CS_UNIT>("white",ColorStyle(false,Qt::white,Qt::white,Qt::black,QColor(56,161,227)))
+        << QPair<CS_UNIT>("blue",ColorStyle(true,QColor(0,0,38),QColor(0,0,50),Qt::white,QColor(85,0,255)))
+        << QPair<CS_UNIT>("darkblue",ColorStyle(true,QColor(14,19,29),QColor(19,25,38),Qt::white,QColor(70,79,89)))
+        << QPair<CS_UNIT>("honeycomb",ColorStyle(false,QColor(185,188,182),QColor(212,215,208),Qt::black,QColor(243,193,41),Qt::white))
+        << QPair<CS_UNIT>("black",ColorStyle(true,Qt::black,QColor(16,16,16),QColor(222,222,222),QColor(132,132,132)))
+        << QPair<CS_UNIT>("darkgreen",ColorStyle(true,QColor(30,30,30),QColor(27,34,36),QColor(197,209,217),QColor(21,67,58)))
+        << QPair<CS_UNIT>("green",ColorStyle(true,QColor(6,29,12),QColor(0,12,0),Qt::white,QColor(86,191,121)))
+        << QPair<CS_UNIT>("stone",ColorStyle(true,QColor(27,36,40),QColor(34,45,50),Qt::white,QColor(165,206,255)));
+    return std::move(map);
+}
+}
 #endif // STYLEHELPER_H
